d69655e44dc676c5c49b6f7d8fb2f667ec259ae0
Martey Jamel <89717401+mjmartey19@users.noreply.github.com>
2022-11-12 21:32:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const menuBar = document.querySelector(".bx-menu");
 const  text = document.querySelectorAll(".text"),
 title = document.querySelector(".title"),
 icon = document.querySelectorAll(".icon"),
- sidebar = document.querySelector(".sidebar");
+sidebar = document.querySelector(".sidebar");
+buttons =  document.querySelectorAll(".link a"),
 menuBar.addEventListener("click", ()=>{
-  sidebar.classList.toggle('active');
+  sidebar.classList.toggle('toggle');
 })
 
+buttons.forEach((button,index) => {
+  button.addEventListener('click', (e)=>{
+    e.preventDefault();
+    button.classList.add('active');
+    removeActive(index);
+  })
+});
 
-var viewMode = getCookie("view-mode");
-if(viewMode == "desktop"){
-    viewport.setAttribute('content', 'width=1024');
-}else if (viewMode == "mobile"){
-    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
-}var viewMode = getCookie("view-mode");
-if(viewMode == "desktop"){
-    viewport.setAttribute('content', 'width=1024');
-}else if (viewMode == "mobile"){
-    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
+function removeActive(index1){
+  buttons.forEach((button,index2)=>{
+    if(index1 != index2){
+      button.classList.remove('active');
+    }
+  })
 }
+
